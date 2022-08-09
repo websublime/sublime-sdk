1c689302ce2e755a0125a9d9fe9db93b4e7865c0
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2022-08-09 14:30:51 +0100

    feat: check commit

--- packages/ws-essential/src/redux.ts
+++ packages/ws-essential/src/redux.ts
@@ -5,50 +5,52 @@
  * found in the LICENSE file at https://websublime.dev/license
  */
 
- import { configureStore, ConfigureStoreOptions, createListenerMiddleware, createReducer, Store } from '@reduxjs/toolkit';
+import { configureStore, ConfigureStoreOptions, createListenerMiddleware, createReducer, Store } from '@reduxjs/toolkit';
 
- type StoreProvider = {
-   options: Partial<ConfigureStoreOptions>,
-   redux: Store | null;
-   listenerMiddleware: ReturnType<typeof createListenerMiddleware>;
- };
- 
- export const { setOptions, useRedux } = (() => {
-   const scope = {
-     options: {},
-     redux: null,
-     listenerMiddleware: createListenerMiddleware()
-   } as StoreProvider;
- 
-   const rootReducer = createReducer({}, (builder) => {
-     builder.addDefaultCase((state) => {
-       return state;
-     });
+type StoreProvider = {
+ options: Partial<ConfigureStoreOptions>,
+ redux: Store | null;
+ listenerMiddleware: ReturnType<typeof createListenerMiddleware>;
+};
+
+const initConfigureStore = () => {
+ const scope = {
+   options: {},
+   redux: null,
+   listenerMiddleware: createListenerMiddleware()
+ } as StoreProvider;
+
+ const rootReducer = createReducer({}, (builder) => {
+   builder.addDefaultCase((state) => {
+     return state;
    });
- 
-   const provider = {
-     setOptions(options: Partial<ConfigureStoreOptions>) {
-       return scope.options = { ...scope.options, ...options };
-     },
-     useRedux() {
-       if(!scope.redux) {
-         scope.redux = configureStore({
-           reducer: rootReducer,
-           middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-             serializableCheck: false
-           }).prepend(scope.listenerMiddleware.middleware),
-           ...scope.options
-         })
-       }
- 
-       return {
-         store: scope.redux,
-         middleware: scope.listenerMiddleware,
-         root: rootReducer,
-         options: scope.options
-       };
+ });
+
+ const provider = {
+   setOptions(options: Partial<ConfigureStoreOptions>) {
+     return scope.options = { ...scope.options, ...options };
+   },
+   useRedux() {
+     if(!scope.redux) {
+       scope.redux = configureStore({
+         reducer: rootReducer,
+         middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+           serializableCheck: false
+         }).prepend(scope.listenerMiddleware.middleware),
+         ...scope.options
+       })
      }
+
+     return {
+       store: scope.redux,
+       middleware: scope.listenerMiddleware,
+       root: rootReducer,
+       options: scope.options
+     };
    }
- 
-   return Object.seal(provider);
- })();
+ }
+
+ return Object.seal(provider);
+};
+
+export const { setOptions, useRedux } = initConfigureStore();
